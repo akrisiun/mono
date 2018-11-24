489975dca17c9476cde5493528234da23879fb27
AndriusK <akrisiun@gmail.com>
2018-11-24 09:59:00 +0200

    jay build + https modules

--- build-libs.bat
+++ build-libs.bat
@@ -1,7 +1,15 @@
 @echo off
 cd mcs\jay
-vcbuild jay.vcxproj
-cd msvc\scripts
-csc prepare.cs
-prepare.exe ..\..\mcs core
+
+@REM # vcbuild 
+msbuild jay.vcxproj
+
+@REM # cd msvc\scripts
+@REM # csc prepare.cs
+@REM # prepare.exe ..\..\mcs core
+cd ..\..
+
+prepare.exe mcs core
+@REM # & .\prepare.exe mcs core
+
 msbuild net_4_x.sln
